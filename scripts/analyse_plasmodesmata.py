a7a26b7a8e0af7914b8d4f0786b2b1b6fe5e7c93
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-12-21 12:07:56 +0000

    Copied get_microscopy_function to analyse_plasmodesmata.py

--- scripts/analyse_plasmodesmata.py
+++ scripts/analyse_plasmodesmata.py
@@ -16,20 +16,34 @@ import warnings
 
 import numpy as np
 
-from jicbioimage.core.image import SegmentedImage
+from jicbioimage.core.image import DataManager, SegmentedImage
 from jicbioimage.core.transform import transformation
-from jicbioimage.core.io import AutoName, AutoWrite
+from jicbioimage.core.io import FileBackend, AutoName, AutoWrite
 from jicbioimage.core.util.array import normalise
 from jicbioimage.core.util.color import pretty_color
 from jicbioimage.segment import connected_components
 from jicbioimage.illustrate import AnnotatedImage
 
-from find_plasmodesmata import get_microscopy_collection
+
+__version__ = "0.6.0"
 
 AutoName.prefix_format = "{:03d}_"
+HERE = os.path.dirname(os.path.realpath(__file__))
 
 # Suppress spurious scikit-image warnings.
 warnings.filterwarnings("ignore", module="skimage.io._io")
+
+
+def get_microscopy_collection(input_file):
+    """Return microscopy collection from input file."""
+    data_dir = os.path.abspath(os.path.join(HERE, "..", "data"))
+    if not os.path.isdir(data_dir):
+        os.mkdir(data_dir)
+    backend_dir = os.path.join(data_dir, 'unpacked')
+    file_backend = FileBackend(backend_dir)
+    data_manager = DataManager(file_backend)
+    microscopy_collection = data_manager.load(input_file)
+    return microscopy_collection
 
 
 @transformation
@@ -60,6 +74,7 @@ def filter_large(segmentation3D, max_voxel):
         else:
             removed[region] = 0
     return segmentation3D, removed
+
 
 def filter_small(segmentation3D, min_voxel):
     """Remove small regions."""
@@ -133,7 +148,6 @@ def plasmodesmata_analysis(microscopy_collection, series, threshold,
     annotate3D(microscopy_collection, series, small_removed)
     AutoName.namespace = ".large.removed."
     annotate3D(microscopy_collection, series, large_removed)
-
 
     # Write out data to CSV files.
     csv_fn = os.path.join(AutoName.directory, "plasmodesmata.csv")
