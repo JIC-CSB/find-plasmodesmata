3e29c0226b48bc7eb444098f7c9e62e9b2beed9c
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-01-08 15:53:16 +0000

    Updated version number.

--- scripts/plasmodesmata_analysis.py
+++ scripts/plasmodesmata_analysis.py
@@ -25,7 +25,7 @@ from jicbioimage.segment import connected_components
 from jicbioimage.illustrate import AnnotatedImage
 
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 AutoWrite.on = False
 HERE = os.path.dirname(os.path.realpath(__file__))
