f7c0d74465dd86f025c3e32af2c5dbcf5f2f4f75
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-01-08 15:52:57 +0000

    Added script for analysing all lif files in an input directory.

---
+++ scripts/analyse_all_images.py
@@ -0,0 +1,68 @@
+"""Analyse all images in an input directory."""
+
+import os
+import os.path
+import argparse
+import logging
+
+from plasmodesmata_analysis import (
+    get_microscopy_collection,
+    __version__
+)
+from analyse_all_series import analyse_all
+
+# Setup logging with a stream handler.
+logger = logging.getLogger(os.path.basename(__file__))
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+def analyse_dir(args):
+    """Analyse all images in an input directory."""
+    for fname in os.listdir(args.input_dir):
+        if not fname.lower().endswith(".lif"):
+            continue
+        logger.info("Analysing image: {}".format(fname))
+
+        dir_name = fname.split(".")[0]
+        specific_out_dir = os.path.join(args.output_dir, dir_name)
+        if not os.path.isdir(specific_out_dir):
+            os.mkdir(specific_out_dir)
+
+        fpath = os.path.join(args.input_dir, fname)
+        microscopy_collection = get_microscopy_collection(fname)
+        analyse_all(microscopy_collection, specific_out_dir, args.threshold,
+                    args.min_voxel, args.max_voxel)
+
+
+def main():
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument("input_dir", help="input directory")
+    parser.add_argument("output_dir", help="output directory")
+    parser.add_argument("-t", "--threshold",
+                        default=15000, type=int,
+                        help="abs threshold (default=20000)")
+    parser.add_argument("--min-voxel", default=2, type=int,
+                        help="Minimum voxel volume (default=2)")
+    parser.add_argument("--max-voxel", default=50, type=int,
+                        help="Maximum voxel volume (default=50)")
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    # Create file handle logger.
+    fh = logging.FileHandler(os.path.join(args.output_dir, "log"), mode="w")
+    fh.setLevel(logging.DEBUG)
+    format_ = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    formatter = logging.Formatter(format_)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    logger.info("Script version: {}".format(__version__))
+
+    analyse_dir(args)
+
+if __name__ == "__main__":
+    main()
